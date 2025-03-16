7619f11cc98c21e7f0e2d91ff447e303e41c81c5
Martin Ondrej <68095300+Gallawander@users.noreply.github.com>
2025-03-16 21:32:59 +0100

    Added columns for disease, medication and regime

--- app/app.py
+++ app/app.py
@@ -14,11 +14,21 @@ SYMPTOMS = [
 st.title("Welcome to a PocketDoctor")
 
 options = st.multiselect(
-    label="",
+    label="Label",
     options=SYMPTOMS,
     placeholder="Start writing your symptoms and then pick from the dropdown list",
+    label_visibility="hidden",
 )
 
 
 if st.button(label="Predict", use_container_width=True):
-    st.write("### Influenza")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write("### :red[Disease]")
+        st.write("##### Influenza")
+    with col2:
+        st.write("### :green[Medication]")
+        st.write("##### Ibuprophene")
+    with col3:
+        st.write("### :blue[Regime]")
+        st.write("##### Rest")
