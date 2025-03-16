b56fb3d9e9d7dafb0d46b1478a35b984808d9b6d
Martin Ondrej <68095300+Gallawander@users.noreply.github.com>
2025-03-16 21:02:31 +0100

    Added multiselct and predict button

--- app/app.py
+++ app/app.py
@@ -1,4 +1,24 @@
 import streamlit as st
 
+SYMPTOMS = [
+    "Running nose",
+    "Fever",
+    "Headache",
+    "Dry cough",
+    "Wet cough",
+    "Shivering",
+    "Rigor",
+]
 
-st.title('Sample APP')
+
+st.title("Welcome to a PocketDoctor")
+
+options = st.multiselect(
+    label="",
+    options=SYMPTOMS,
+    placeholder="Start writing your symptoms and then pick from the dropdown list",
+)
+
+
+if st.button(label="Predict", use_container_width=True):
+    st.write("### Influenza")
